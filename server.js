51c8e21e10dee061e85b26c692cf9e63986acf23
Ruben Ribeiro <rubenribeiro@outlook.com>
2021-04-15 20:22:07 -0400

    Update CORS Allow Control Origin Paramater

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ connectDB();
 
 // Configure CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
